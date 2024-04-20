4f2726e7868f137c0009cc2b7476e3145c61f71e
Patrick Silva <patrickeeep12@gmail.com>
2024-04-20 18:59:32 -0300

    feat: ensure Client can get stats about your photos

--- src/app/conta/estatisticas/page.tsx
+++ src/app/conta/estatisticas/page.tsx
@@ -1,13 +1,25 @@
+import statsGet from '@/actions/stats-get';
 import { Metadata } from 'next';
+import dynamic from 'next/dynamic';
+
+const ContaEstatisticas = dynamic(
+  () => import('@/components/conta/conta-estatisticas'),
+  {
+    loading: () => <p>Carregando...</p>,
+    ssr: false,
+  },
+);
 
 export const metadata: Metadata = {
   title: 'Estatísticas | Minha conta',
 };
 
 export default async function EstatisticasPage() {
+  const { data } = await statsGet();
+  if (!data) return null;
   return (
-    <main>
-      <h1>Estatisticas</h1>
-    </main>
+    <section>
+      <ContaEstatisticas data={data} />
+    </section>
   );
 }
