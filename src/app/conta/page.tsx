167926cd7949610a1fe0f29215aecc2762de468d
Patrick Silva <patrickeeep12@gmail.com>
2024-04-20 08:29:37 -0300

    feat: create my account client where he shows your photos

--- src/app/conta/page.tsx
+++ src/app/conta/page.tsx
@@ -1,13 +1,37 @@
+import photosGet from '@/actions/photos-get';
+import userGet from '@/actions/user-get';
+import Feed from '@/components/feed/feed';
 import { Metadata } from 'next';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: 'Minha conta',
 };
 
-export default function ContaPage() {
+export default async function ContaPage() {
+  const { data: user } = await userGet();
+  const { data } = await photosGet({ user: user?.username });
+
   return (
     <section>
-      <h1>Conta</h1>
+      {data?.length ? (
+        <Feed photos={data} />
+      ) : (
+        <div>
+          <p
+            style={{ color: '#444', fontSize: '1.25rem', marginBottom: '1rem' }}
+          >
+            Nenhuma foto encontrda.
+          </p>
+          <Link
+            href="/conta/postar"
+            className="button"
+            style={{ display: 'inline-block' }}
+          >
+            Postar Foto
+          </Link>
+        </div>
+      )}
     </section>
   );
 }
--- src/app/conta/postar/page.tsx
+++ src/app/conta/postar/page.tsx
@@ -1,7 +1,7 @@
 import ContaPhotoPost from '@/components/conta/conta-photo-post';
 import { Metadata } from 'next';
 
-export const runtime = 'edge';
+// export const runtime = 'edge';
 
 export const metadata: Metadata = {
   title: 'Portar | Minha conta',
